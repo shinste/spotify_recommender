02f71ef47c585a8e1687b8eaa32b1bd67e235b05
shinste <stephenshin1@hotmail.com>
2024-07-06 19:34:39 -0700

    mid-fix

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -1,36 +1,52 @@
 import axios from 'axios';
 import Display from "./Display";
-import Playlist from '../icons/playlist.png'
-import Remove from '../icons/remove.png'
+import Playlist from '../icons/playlist.png';
+import Remove from '../icons/remove.png';
 
 interface RecommendationProps {
-    order: string[],
-    setOrder: React.Dispatch<React.SetStateAction<string[]>>,
-    token: string,
-    allIds: string[],
-    positions: {[key: string]: {[key: string]: string}},
-    setPositions: React.Dispatch<React.SetStateAction<{[key: string]: {[key: string]: string}}>>,
-    setAllIds: React.Dispatch<React.SetStateAction<string[]>>,
-    recommended: any[],
-    setRecommended: React.Dispatch<React.SetStateAction<any[]>>,
-    setSelected: React.Dispatch<React.SetStateAction<string>>,
-    mute: boolean,
-    setMute: React.Dispatch<React.SetStateAction<boolean>>,
-    handleButtonClick: (uri: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>, name: string) => void,
-    handleDrag: (e: React.DragEvent, id: string, title: string, type: string, image: string, uri: string) => void,
-    handleAdd: (id: string, title: string, type: string, url: string) => void
-
+    order: string[];
+    setOrder: React.Dispatch<React.SetStateAction<string[]>>;
+    token: string;
+    allIds: string[];
+    positions: { [key: string]: { [key: string]: string } };
+    setPositions: React.Dispatch<React.SetStateAction<{ [key: string]: { [key: string]: string } }>>;
+    setAllIds: React.Dispatch<React.SetStateAction<string[]>>;
+    recommended: any[];
+    setRecommended: React.Dispatch<React.SetStateAction<any[]>>;
+    setSelected: React.Dispatch<React.SetStateAction<string>>;
+    mute: boolean;
+    setMute: React.Dispatch<React.SetStateAction<boolean>>;
+    handleButtonClick: (uri: string, e: React.MouseEvent<HTMLButtonElement, MouseEvent>, name: string) => void;
+    handleDrag: (e: React.DragEvent, id: string, title: string, type: string, image: string, uri: string) => void;
+    handleAdd: (id: string, title: string, type: string, url: string) => void;
 }
-const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, allIds, positions, setPositions, setAllIds, recommended, setRecommended, setSelected, mute, setMute, handleButtonClick, handleDrag, handleAdd}) => {
+
+const Recommendation: React.FC<RecommendationProps> = ({
+    token,
+    order,
+    setOrder,
+    allIds,
+    positions,
+    setPositions,
+    setAllIds,
+    recommended,
+    setRecommended,
+    setSelected,
+    mute,
+    setMute,
+    handleButtonClick,
+    handleDrag,
+    handleAdd
+}) => {
 
     const recommend = async () => {
-        const seedSongs = Object.entries(positions).map(([key, value]) => 
+        const seedSongs = Object.entries(positions).map(([key, value]) =>
             value.type === "track" && value.seedId).filter(Boolean);
-        
-        const seedArtists = Object.entries(positions).map(([key, value]) => 
+
+        const seedArtists = Object.entries(positions).map(([key, value]) =>
             value.type === "artist" && value.seedId).filter(Boolean);
         try {
-            const {data} = await axios.get('https://api.spotify.com/v1/recommendations', {
+            const { data } = await axios.get('https://api.spotify.com/v1/recommendations', {
                 headers: {
                     Authorization: `Bearer ${token}`
                 },
@@ -39,46 +55,41 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
                     seed_artists: seedArtists.join(','),
                     limit: 20
                 }
-            })
+            });
             setRecommended(data.tracks);
-            setSelected('All')
+            setSelected('All');
         } catch (error) {
             console.log(error);
         }
     };
 
     const handleDrop = (e: React.DragEvent, spot: string) => {
-        
         const seedTitle = e.dataTransfer.getData('title') as string;
-        
         const seedId = e.dataTransfer.getData('id') as string;
         const imageUrl = e.dataTransfer.getData('image') as string;
         const type = e.dataTransfer.getData('type') as string;
         const imageSpot = document.getElementById(spot) as HTMLImageElement;
-        let newPositions = {...positions};
-        let newIds = [...allIds]
+        let newPositions = { ...positions };
+        let newIds = [...allIds];
         if (!allIds.includes(seedId)) {
             if (imageSpot) {
                 if (Object.keys(positions[spot]).length > 0) {
                     newIds = allIds.filter((value) => value !== positions[spot].url);
                     newPositions = Object.fromEntries(
-                        Object.entries(positions).map(([key, value]) => 
+                        Object.entries(positions).map(([key, value]) =>
                             key === spot ? [key, []] : [key, value]
                         )
                     );
-                    
                 }
                 setPositions(Object.fromEntries(
-                    Object.entries(newPositions).map(([key, value]) => 
-                        key === spot ? [key, {url: imageUrl, seedId: seedId, type: type, title: seedTitle}] : [key, value]
+                    Object.entries(newPositions).map(([key, value]) =>
+                        key === spot ? [key, { url: imageUrl, seedId: seedId, type: type, title: seedTitle }] : [key, value]
                     )
                 ));
             }
             setAllIds([...newIds, seedId]);
         }
-        
     }
-
 
     const handleDragOver = (e: React.DragEvent) => {
         e.preventDefault();
@@ -88,9 +99,9 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
         setPositions(Object.fromEntries(
             Object.entries(positions).map(([key, value]) =>
                 key === position ? [key, {}] : [key, value]
-            ) 
-        ))
-        setAllIds(allIds.filter((value) => value !== positions[position].seedId))
+            )
+        ));
+        setAllIds(allIds.filter((value) => value !== positions[position].seedId));
     }
 
     const handleHover = (index: number, status: boolean) => {
@@ -99,7 +110,6 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
         if (addRemoveButton && playlistButton) {
             addRemoveButton.hidden = status;
             playlistButton.hidden = status;
-
         }
     }
 
@@ -110,7 +120,6 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
             newPositions.unshift(String(first));
             return newPositions;
         });
-        
     }
 
     const handleRight = () => {
@@ -123,11 +132,9 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
     }
 
     return (
-        <div id='Recommendation-div'>
+        <div id='Recommendation-div' className='horizontal-center'>
             <h4>Recommendation Customizer</h4>
-            <div className="Flex" style={{marginBottom: '80px'}}>
-                <div className="Vertical-flex" style={{overflowY: 'auto'}}>
-            </div>
+            <div className="Vertical-flex" style={{ marginBottom: '80px' }}>
                 <div id="Main-display">
                     {allIds.length > 0 &&
                         <button id="Left-button" onClick={handleLeft}>&lt;</button>
@@ -135,23 +142,23 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
                     {order.map((position, index) => {
                         return (
                             <div onMouseEnter={() => handleHover(index, false)} onMouseLeave={() => handleHover(index, true)} className={'display' + String(index) + " Vertical-center Hover-div"} onDrop={(e) => handleDrop(e, position)} onDragOver={handleDragOver}>
-                                <img id={position} className='Recommend-img' src={Object.keys(positions[position]).length > 0 ? positions[position].url : undefined} alt=""/>
-                                {allIds.length === 0 && index === 2 && 
-                                            <div>
-                                                Drag or manually add songs/tracks here!
-                                            </div>
-                                        }
+                                <img id={position} className='Recommend-img' src={Object.keys(positions[position]).length > 0 ? positions[position].url : undefined} alt="" />
+                                {allIds.length === 0 && index === 2 &&
+                                    <div>
+                                        Drag or manually add songs/tracks here!
+                                    </div>
+                                }
                                 {Object.keys(positions[position]).length > 0 && index === 2 &&
                                     <div className="Button-hover">
-                                        <button id={'Remove-Add-' + String(index)}hidden style={{marginRight: '15px', backgroundColor: 'grey'}} onClick={() => handleRemove(position)}>
-                                            <img src={Remove} className='Hover-button' alt=""/>
+                                        <button id={'Remove-Add-' + String(index)} hidden style={{ marginRight: '15px', backgroundColor: 'grey' }} onClick={() => handleRemove(position)}>
+                                            <img src={Remove} className='Hover-button' alt="" />
                                         </button>
-                                        <button id={'Playlist-' + String(index)} className="hs-dropdown-toggle" hidden style={{marginLeft: '15px', backgroundColor: 'grey'}}>
-                                            <img src={Playlist} className='Hover-button' alt=""/>
+                                        <button id={'Playlist-' + String(index)} className="hs-dropdown-toggle" hidden style={{ marginLeft: '15px', backgroundColor: 'grey' }}>
+                                            <img src={Playlist} className='Hover-button' alt="" />
                                         </button>
                                     </div>
                                 }
-                                {positions[position].title}
+                                <p style={{ color: 'whitesmoke', margin: 0}}>{positions[position].title}</p>
                             </div>
                         );
                     })}
@@ -159,20 +166,26 @@ const Recommendation: React.FC<RecommendationProps> = ({token, order, setOrder, 
                         <button id="Right-button" onClick={handleRight}>&gt;</button>
                     }
                 </div>
+                <div className='horizontal-center results' style={{marginTop: '30px'}}>
+                    {recommended.length > 0 && <button onClick={() => {
+                        setAllIds([]);
+                        setPositions({ "imageDrop1": {}, "imageDrop2": {}, "imageDrop3": {}, "imageDrop4": {}, "imageDrop5": {} });
+                    }}>Clear Seed Songs/Artists</button>}
+                    {recommended.length === 0 &&
+                        <button onClick={recommend} className="py-3 px-4 gap-x-2 mb-5 text-sm font-semibold rounded-lg border border-transparent bg-purple-600 text-white hover:bg-purple-700">
+                            Create Recommendation
+                        </button>
+                    }
+                    <div className='horizontal-center' style={{ width: '800px' }}>
+                        {recommended.length > 0 && <Display showcase={recommended} title={'Recommended Songs'} reference={'recommended'} setMute={setMute} mute={mute} handleButtonClick={handleButtonClick} handleDrag={handleDrag} handleAdd={handleAdd} />}
+                    </div>
+                    {recommended.length > 0 &&
+                        <button onClick={recommend} className="py-3 px-4 gap-x-2 mb-5 text-sm font-semibold rounded-lg border border-transparent bg-purple-600 text-white hover:bg-purple-700">
+                            Generate New Recommendations
+                        </button>}
+                </div>
             </div>
-            {recommended.length > 0 && <button onClick={() => {setAllIds([]);
-                                                            setPositions({"imageDrop1": {}, "imageDrop2": {}, "imageDrop3": {}, "imageDrop4": {}, "imageDrop5": {}})}}>Clear Seed Songs/Artists</button>}
-            {recommended.length === 0 && 
-                <button onClick={recommend} className="py-3 px-4 gap-x-2 mb-5 text-sm font-semibold rounded-lg border border-transparent bg-purple-600 text-white hover:bg-purple-700">
-                    Create Recommendation
-                </button>
-            }
-            {recommended.length > 0 &&<Display showcase={recommended} title={'Recommended Songs'} reference={'recommended'} setMute={setMute} mute={mute} handleButtonClick={handleButtonClick} handleDrag={handleDrag} handleAdd={handleAdd}/>}
-            {recommended.length > 0 && 
-                <button onClick={recommend} className="py-3 px-4 gap-x-2 mb-5 text-sm font-semibold rounded-lg border border-transparent bg-purple-600 text-white hover:bg-purple-700">
-                    Generate New Recommendations
-                </button> }
-    </div>
+        </div>
     );
 }
 
