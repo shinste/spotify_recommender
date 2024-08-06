75a33cc00f3cd537de75912dcfa1a92bb2ac7a0b
shinste <stephenshin1@hotmail.com>
2024-08-06 15:38:51 -0700

    fix login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,10 +9,23 @@ const Login = () => {
     return (
         <div id="Login-body">
             <div style={{ paddingTop: '140px' }} className="Flex">
-                <div id="Title-div">
-                    <h3 id="Login-title">Spotify Song Recommender</h3>
-                    <h3 id="Login-label">Customize your recommendations</h3>
+                <div className='Vertical-flex'>
+                    <div id="Title-div">
+                        <h3 id="Login-title">Spotify Song Recommender</h3>
+                        <h3 id="Login-label">Customize your recommendations</h3>
+                    </div>
+                    <div id="Disclaimer">
+                        <h5 className="mt-0 mb-0" style={{ fontSize: '30px' }}>What is being authorized on my account?</h5>
+                        <p>To create the best possible experience with this application, certain permissions are required.
+                            These include access to your profile information, playlists, top songs and artists, and more.
+                            The primary use of Spotify's API in this application is to view your data. The only action that
+                            will modify your account is the ability to add selected songs to your existing playlists.
+                            The link on the left will direct you to Spotify's authorization disclaimer page, where you can
+                            see a detailed list of the information your account will share.
+                        </p>
+                    </div>
                 </div>
+
                 <div id="Gif-holder">
                     <iframe className="No-hover" src="https://giphy.com/embed/mCyRzRJINnNUb62vLb" width="480" height="480" frameBorder="0" title="SpotifyGif"></iframe>
                     <p id='important'>IMPORTANT: Use testing credentials: <span style={{ display: 'block', fontWeight: 'bolder' }}>[ username: stephenshin111@gmail.com | password: spotifytesting1! ]</span> if you have not added your email to this whitelist.
@@ -25,19 +38,6 @@ const Login = () => {
                         </a>
                     </div>
                 </div>
-            </div>
-            <div className="Flex">
-                <div id="Disclaimer">
-                    <h5 className="mt-0 mb-0" style={{ fontSize: '30px' }}>What is being authorized on my account?</h5>
-                    <p>To create the best possible experience with this application, certain permissions are required.
-                        These include access to your profile information, playlists, top songs and artists, and more.
-                        The primary use of Spotify's API in this application is to view your data. The only action that
-                        will modify your account is the ability to add selected songs to your existing playlists.
-                        The link on the left will direct you to Spotify's authorization disclaimer page, where you can
-                        see a detailed list of the information your account will share.
-                    </p>
-                </div>
-
             </div>
 
         </div>
